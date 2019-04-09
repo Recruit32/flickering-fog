ab8a798f9f71ec3d3bf42d2ac345059bc5c3ad20
Recruit32 <recruit.a32@gmail.com>
2019-04-09 16:29:19 -0700

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCYA-9AV2xFPbx9rQFykSkgklzS3l6Qgfk",
+    authDomain: "flickering-fog.firebaseapp.com",
+    databaseURL: "https://flickering-fog.firebaseio.com",
+    projectId: "flickering-fog",
+    storageBucket: "flickering-fog.appspot.com",
+    messagingSenderId: "18032779050"
+  }
 };
